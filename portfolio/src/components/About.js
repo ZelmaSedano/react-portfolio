6fb274437a97833af5284e5f9d094365b94c2ea7
TPL_7_2021H2 <tpl_7_2021h2@Zelma.local>
2021-10-04 14:04:25 -0500

    fixing grammar

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -12,7 +12,7 @@ function About() {
 
         <p className='about-p-small'>Chicago via Oslo, Norway via Mississippi. I am a Southern LatinX who looks for beauty in all things, the wonder in every story, and the next big idea.</p>
 
-        <p className='about-p-small'>I believe my background in Psychology is what makes me a impactful designer, and my experience in Finance makes me an efficient developer.</p>
+        <p className='about-p-small'>I believe my background in Psychology is what makes me an impactful designer, and my experience in Finance makes me an efficient developer.</p>
 
         <p class='say-hi'>Say hi on Social Media!</p>
 
